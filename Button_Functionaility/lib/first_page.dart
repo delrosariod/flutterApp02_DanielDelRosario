1731be2dc06dd88c20ad1e717c7eb2037234988b
delrosariod <59617888+delrosariod@users.noreply.github.com>
2020-10-18 17:01:01 -0600

    Button Created and counter initiated

--- Button_Functionaility/lib/first_page.dart
+++ Button_Functionaility/lib/first_page.dart
@@ -8,45 +8,41 @@ class MyFirstPage extends StatefulWidget {
 class _MyFirstPageState extends State<MyFirstPage> {
   bool _enabled = false;
   String _msg1 = '';
+  String _msg2 = '';
+  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
     Object onPressed1() {
       if (_enabled) {
-        setState(() {
-          _msg1 = 'Enabled';
-        });
-        print('onPressed1 returning address of anon func but NOT running it');
         return () {
-          print('Anon func now running as button pressed');
+          setState(() {
+            _count++;
+            print('count is $_count');
+          },
+          );
         };
       } else {
-        setState(() {
-          _msg1 = '';
-        });
-        print('onPressed1 returning NULL');
         return null;
       }
     }
 
-    Object onPressed2() {
-      if (_enabled) {
-        print(
-            'onPressed2 returning the result of running the anonymous function');
-        return () {
-          print('Anon func now running');
-        }();
-      } else {
-        print('onPressed2 returning NULL');
-        return null;
-      }
-    }
+    // Object resetCounter() {
+    //   if (_enabled) {
 
-    print('The build is being RUN');
+    //     return () {
+    //       setState(() {
+
+    //       });
+    //     }();
+    //   } else {
+    //     return null;
+    //   }
+    // }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Button Functionality Demo'),
+        title: Text('Button Functionality'),
       ),
       body: Container(
         child: Column(
@@ -90,6 +86,20 @@ class _MyFirstPageState extends State<MyFirstPage> {
                     child: Text(_msg1),
                   ),
                 ),
+                Container(
+                    margin: EdgeInsets.all(20),
+                    child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      elevation: 8,
+                      textColor: Colors.red.shade100,
+                      color: Colors.blueAccent,
+                      highlightColor: Colors.red,
+                      splashColor: Colors.green.shade300,
+                      padding: EdgeInsets.all(20.0),
+                      onPressed: onPressed1(),
+                      child: Text(_msg1),
+                    )),
               ],
             ),
           ],
